ddcada187aeac848eef2a352be4a1b934565fb22
Kartik Hallikeri <111274638+ka6tik@users.noreply.github.com>
2023-11-22 17:42:43 +0530

    Create finalcheck.groovy

---
+++ vars/finalcheck.groovy
@@ -0,0 +1,25 @@
+def call() {
+    // Define tools and libraries
+    def buildTools = tool name: 'maven', type: 'maven'
+    def libraries = library 'your-library'
+
+    // Checkout code
+    stage('Checkout') {
+        echo "Git Checkout"
+    }
+
+    // Build
+    stage('Build') {
+        sh "${buildTools}/bin/mvn clean install"
+    }
+
+    // Deploy
+    stage('Deploy') {
+        // Add your deploy commands here
+    }
+
+    // Archive
+    stage('Archive') {
+        archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true
+    }
+}
